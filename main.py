368a0501417a563d23bdfbdf60a4da26094d7863
mahendra soni <mahendrasoni25122002@gmail.com>
2025-12-16 13:55:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -301,13 +301,6 @@ def delete_product(pid):
     finally:
         conn.close()
 
-if **name** == "**main**":
-    from waitress import serve
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    host = "0.0.0.0" # Explicitly set
-    print(f"Pukhraj Herbal Admin starting on http://{host}:{port}/admin-login")
-    serve(app, host=host, port=port, url_scheme='https')
 
 
 
